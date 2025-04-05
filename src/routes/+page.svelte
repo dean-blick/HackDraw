8bb5810e3e17e45a2888b39f1fdcf5faee19eee3
dean-blick <steveworkless@gmail.com>
2025-04-04 23:32:14 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { Segment } from '@skeletonlabs/skeleton-svelte';
+    import { draw } from "svelte/transition";
 
     let size = $state('sm');
 
     let width: number = $state(0)
     let pixels: Array<Array<string>> = $state([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],])
     let isScreenLarge: boolean = $derived(width > 1023)
+
+    let zoomSizes: Array<number> = $state([2, 3, 4, 5, 6])
+    let zoomLevel: number = $state(0)
 
     async function GetPixels() {
         let response = await fetch('/')
@@ -18,7 +22,28 @@
         });
         await setTimeout(() => {
             GetPixels()
-        }, 500);
+        }, 750);
+    }
+
+    let activeColor: string = $state("#000000")
+
+    async function sendPixel(i: number) {
+        pixels[Math.floor(i/100)][i % 100] = activeColor
+        const response = await fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',       
+            },
+            body: JSON.stringify({index: i, color: activeColor}), 
+        });
+    }
+
+    function zoomIn() {
+        if (zoomLevel < 4) zoomLevel = zoomLevel + 1
+    }
+
+    function zoomOut() {
+        if (zoomLevel > 0) zoomLevel = zoomLevel - 1
     }
 
     onMount(() => {
@@ -29,17 +54,18 @@
 
 <svelte:window bind:innerWidth={width}/>
 <div class="flex flex-col lg:flex-row">
-    <div class="card bg-surface-100-900 flex flex-row lg:flex-col h-screen">
+    <div class="bg-surface-100-900 flex flex-row lg:flex-col h-screen w-[120px]">
         <Segment name="size" value={size} onValueChange={(e) => (size = e.value!)} orientation="vertical">
             <Segment.Item value="sm">Small</Segment.Item>
             <Segment.Item value="md">Medium</Segment.Item>
             <Segment.Item value="lg">Large</Segment.Item>
-          </Segment>
+        </Segment>
+        <button onclick={() => zoomIn()} disabled={zoomLevel == 4}>Zoom In</button>
+        <button onclick={() => zoomOut()} disabled={zoomLevel == 0}>Zoom Out</button>
     </div>
-</div>
-<div class="grid grid-rows-100 grid-cols-100">
-    {#each Array(10000) as _, i}
-        <button style="backgroundColor: {pixels[Math.floor(i/100)][i % 100]}" class="w-4 h-4" aria-label="pixel"></button>
-    {/each}
-
+    <div class="grid grid-rows-100 grid-cols-100 h-screen w-1/2 overflow-scroll">
+        {#each Array(10000) as _, i}
+            <button onclick={() => sendPixel(i)} style="background-color: {pixels[Math.floor(i/100)][i % 100]}" class="w-{zoomSizes[zoomLevel]} h-{zoomSizes[zoomLevel]} border-[1px] border-gray-300" aria-label="pixel"></button>
+        {/each}
+    </div>
 </div>
--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -12,3 +12,25 @@ export async function GET(): Promise<Response> {
         }
     });
 }
+
+export async function POST({request}): Promise<Response> {
+    let body = await request.json()
+    const filter = { index: Math.floor(body.index / 100) }; // Find the document with _id 1
+    
+    const data = (await canvasData.find(filter).toArray()).map(e => ({
+        ...e
+    }))
+    let doc = data[0]
+    let newPixels = doc.pixels
+    newPixels[body.index%100] = body.color
+    const updateDoc = {
+        $set: { pixels: newPixels },
+    };
+    await canvasData.updateOne(filter, updateDoc)
+
+    return new Response(JSON.stringify(data), {
+        headers: {
+            'content-type': 'application/json',
+        }
+    });
+}
